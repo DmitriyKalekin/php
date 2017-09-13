404749d3a97db4b5da5c4d63adabc8ca8abf1656
KalekinDmitry <herr_horror@mail.ru>
2017-09-13 09:47:08 +0300

    Create Controller.class.php

---
+++ core/Controller.class.php
@@ -0,0 +1,15 @@
+<?php
+class Controller 
+{
+  public function getHeader()
+  {
+    return "Content-type: text/json;";
+  }
+  
+  public function getPage()
+  {
+    echo "Impossible entrance";
+  }
+  
+}
+?>
